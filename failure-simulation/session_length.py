17f27f9f31ab753b46fdbaee81aa03ec65b637cc
ralph-hatoum <hatoumralph@gmail.com>
2023-12-12 15:49:37 +0100

    weibull failure model - chart - python script

--- failure-simulation/session_length.py
+++ failure-simulation/session_length.py
@@ -1,8 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-shape = 1.75  # Adjust for heavy or light tail
-scale = 2.0  # Adjust for overall scale
+shape = 1.5  # Adjust for heavy or light tail
 
 # Number of sessions to generate
 num_sessions = 100000  # Adjust as needed
@@ -20,11 +19,43 @@ def plot_weibull():
 
 
 def simulate_failure(nb_epochs):
+    
+    c_f = 0.75
+
+    x_values = []
+    y_values = []
+
     t=0
-    while t<nb_epochs:
-        wait_time = np.random.weibull(shape)
-        print(wait_time)
-        t = nb_epochs
+
+    scale = 100
+
+    while t<nb_epochs: 
+
+        session_length = get_session_length_in_epochs(shape, scale)
+
+        downtime = get_downtime_from_session_length(session_length, c_f)
+
+        for _ in range(session_length):
+            x_values.append(t)
+            t+=1
+            y_values.append(1)
+
+        for _ in range(downtime):
+            x_values.append(t)
+            t+=1
+            y_values.append(0)
+    
+    plt.xlabel("Epoch")
+    plt.ylabel("Up or down")
+    plt.plot(x_values, y_values,"x")
+    plt.title("Failure chart (Weibull model)")
+    plt.show()          
+
+def get_downtime_from_session_length(session_length, c_f):
+    return int(((1-c_f)/c_f)*session_length)
+
+def get_session_length_in_epochs(shape, scale):
+    return int(np.random.weibull(shape) * scale)
 
 
 def node_availability_graph(nb_epochs):
@@ -34,4 +65,4 @@ def node_availability_graph(nb_epochs):
         availability = np.random.weibull(shape, num_sessions) * scale
         
 
-simulate_failure(100)
+simulate_failure(1000)
