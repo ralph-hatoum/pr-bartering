2ce98567cf1d228bb7c12b9ecb9aa28c541cfd0b
ralph-hatoum <hatoumralph@gmail.com>
2023-12-13 18:22:03 +0100

    wrote test for ExtractFailureModeNodeProfile ; test passed

--- failure-simulation/failure-simulation_test.go
+++ failure-simulation/failure-simulation_test.go
@@ -2,14 +2,21 @@ package failuresimulation
 
 import (
 	configextractor "bartering/config-extractor"
+	"reflect"
 	"testing"
 )
 
 func TestExtractFailureModelNodeProfile(t *testing.T) {
 
 	config1 := configextractor.Config{FailureModel: "weibull"}
+	config2 := configextractor.Config{FailureModel: "lognormal"}
 
-	probabilityLaw, err := ExtractFailureModelNodeProfile(config1)
+	probabilityLaw1, _ := ExtractFailureModelNodeProfile(config1)
+	probabilityLaw2, _ := ExtractFailureModelNodeProfile(config2)
+
+	if reflect.ValueOf(probabilityLaw1).Pointer() != reflect.ValueOf(DrawNumberWeibull).Pointer() && reflect.ValueOf(probabilityLaw2).Pointer() != reflect.ValueOf(DrawNumberLognormal).Pointer() {
+		t.Errorf("function not returning right probability law drawing function")
+	}
 }
 
 func TestDrawNumberWeibull(t *testing.T) {
