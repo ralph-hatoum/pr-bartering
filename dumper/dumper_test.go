c1e1821db7ba88adc4d599b0838eab38d1e05886
ralph-hatoum <hatoumralph@gmail.com>
2024-11-19 22:22:30 +0100

    refactor dumper tests

--- dumper/dumper_test.go
+++ dumper/dumper_test.go
@@ -10,16 +10,16 @@ import (
 )
 
 func init() {
-	bytesAtPeers := []datastructures.PeerStorageUse{datastructures.PeerStorageUse{NodeIP: "peer1", StorageAtNode: 21.0}}
-	bytesForPeers := []datastructures.PeerStorageUse{datastructures.PeerStorageUse{NodeIP: "peer2", StorageAtNode: 23.0}}
-	fulfilledRequests := []datastructures.FulfilledRequest{datastructures.FulfilledRequest{CID: "TestCID", Peer: "peer2", FileSize: 10.0}}
+	bytesAtPeers := []datastructures.PeerStorageUse{{NodeIP: "peer1", StorageAtNode: 21.0}}
+	bytesForPeers := []datastructures.PeerStorageUse{{NodeIP: "peer2", StorageAtNode: 23.0}}
+	fulfilledRequests := []datastructures.FulfilledRequest{{CID: "TestCID", Peer: "peer2", FileSize: 10.0}}
 	storagePool := []string{"pool1", "pool2"}
-	pendingRequests := []datastructures.StorageRequest{datastructures.StorageRequest{CID: "TestCID", FileSize: 11.0}}
+	pendingRequests := []datastructures.StorageRequest{{CID: "TestCID", FileSize: 11.0}}
 	peers := []string{"peer1", "peer2", "peer3"}
-	scores := []datastructures.NodeScore{datastructures.NodeScore{NodeIP: "testIP", Score: 10.0}}
-	ratiosForPeers := []datastructures.NodeRatio{datastructures.NodeRatio{NodeIP: "testIP", Ratio: 1.0}}
-	ratiosAtPeers := []datastructures.NodeRatio{datastructures.NodeRatio{NodeIP: "testIP", Ratio: 2.0}}
-	storedForPeers := []datastructures.FulfilledRequest{datastructures.FulfilledRequest{CID: "TestCID", Peer: "peer2", FileSize: 10.0}, datastructures.FulfilledRequest{CID: "TestCID2", Peer: "peer3", FileSize: 13.0}}
+	scores := []datastructures.NodeScore{{NodeIP: "testIP", Score: 10.0}}
+	ratiosForPeers := []datastructures.NodeRatio{{NodeIP: "testIP", Ratio: 1.0}}
+	ratiosAtPeers := []datastructures.NodeRatio{{NodeIP: "testIP", Ratio: 2.0}}
+	storedForPeers := []datastructures.FulfilledRequest{{CID: "TestCID", Peer: "peer2", FileSize: 10.0}, {CID: "TestCID2", Peer: "peer3", FileSize: 13.0}}
 
 	go Dumper(bytesAtPeers, bytesForPeers, fulfilledRequests, storagePool, pendingRequests, peers, scores, ratiosForPeers, ratiosAtPeers, storedForPeers)
 }
